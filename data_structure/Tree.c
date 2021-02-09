a7e219008b9b10a1af6effc262b873c2277a1ac5
lyzx_hero <hero@MacBook-Pro.local>
2021-02-09 19:36:54 +0800

    中序遍历的非递归遍历方法(未完成)

--- data_structure/Tree.c
+++ data_structure/Tree.c
@@ -8,11 +8,30 @@ typedef struct Node{
     struct Node * rightNode;
 }Node,*PNode;
 
+typedef struct Element{
+    PNode pNode;
+    struct Element * nextElement;
+    struct Element * prevElement;
+}Element,*PElement;
+
 PNode initNode(int number);
 void preOrder(PNode node);
 void showNode(PNode node);
 void inOrder(PNode node);
 void postOrder(PNode node);
+
+typedef struct LinkedStack{
+       PElement top;
+       PElement bottom;
+       int size;
+}LinkedStack,*PLinkedStack;
+PLinkedStack initLinkedStack();
+PElement initElement(PNode node);
+PLinkedStack pushStack(PLinkedStack pStack,PElement ele);
+char checkStack(PLinkedStack pStack);
+PElement pop(PLinkedStack pStack);
+void inOrderNonRecursion(PNode root);
+
 
 
 /**
@@ -47,8 +66,9 @@ int main(void){
     node11->rightNode = node21;
 
     // preOrder(root);
-    //inOrder(root);
-    postOrder(root);
+    // inOrder(root);  //4  11  21  10  12  0  8  9  99
+    // postOrder(root);
+    inOrderNonRecursion(root); //4  11  21  10  12  0  8  9  99  4  11  10  0
 }
 
 void preOrder(PNode node){
@@ -88,4 +108,78 @@ PNode initNode(int number){
 
 void showNode(PNode node){
     printf(" %d ",node->number);
+}
+
+
+/**
+ * 中序遍历的非递归遍历形式(未完)//todo
+ * 
+ **/
+void inOrderNonRecursion(PNode root){
+    PLinkedStack pStack = initLinkedStack();
+    PNode tempNode = root;
+    while(NULL != tempNode || 0 != pStack->size){
+        if(tempNode){
+            pushStack(pStack,initElement(initNode(tempNode->number)));
+            tempNode= tempNode->leftNode;
+        }else{
+            PElement pEle = pop(pStack);
+            showNode(pEle->pNode);
+            tempNode = pEle->pNode->rightNode;
+        }
+    }
+}
+
+PLinkedStack initLinkedStack(){
+    PLinkedStack pStack = (PLinkedStack)malloc(sizeof(LinkedStack));
+    PElement pHead = initElement(initNode(0));
+    pStack->top = pHead;
+    pStack->bottom = pHead;
+    pStack->size = 0;
+    pHead->nextElement=NULL;
+    pHead->prevElement=NULL;
+    return pStack;
+}
+
+PLinkedStack pushStack(PLinkedStack pStack,PElement ele){
+    pStack->top->nextElement=ele;
+    ele->prevElement=pStack->top;
+
+    pStack->top=ele;
+    ele->nextElement=NULL;
+    pStack->size = pStack->size + 1;
+    return pStack;
+}
+
+PElement initElement(PNode node){
+    PElement ele = (PElement)malloc(sizeof(Element));
+    ele->pNode=node;
+    ele->nextElement=NULL;
+    ele->prevElement=NULL;
+    return ele;
+}
+
+char checkStack(PLinkedStack pStack){
+    if(NULL == pStack){
+        printf("这是一个空栈!不给展示！");
+        return 'F';
+    }
+    return 'M';
+}
+
+PElement pop(PLinkedStack pStack){
+    char checkResult = checkStack(pStack);
+    if('F' == checkResult){
+        return NULL;
+    }
+
+    PElement pele = pStack->top;
+    pStack->top=pele->prevElement;
+
+    pele->prevElement->nextElement=NULL;
+    pele->prevElement=NULL;
+
+    pStack->size = pStack->size - 1;
+
+    return pele;
 }
