535580e9b33d4c3c6bb70f3ababb89f2adbafd85
lyzx_hero <hero@MacBook-Pro.local>
2021-02-16 10:07:27 +0800

    邻接矩阵表示的图的广度遍历完成

--- data_structure/GraphBFSWithMatrix.c
+++ data_structure/GraphBFSWithMatrix.c
@@ -8,8 +8,30 @@ typedef struct {
     int vaildNodeNum;
 }MatrixGraph,*PMatrixGraph;
 
+typedef struct Element{
+    int id;
+    struct Element * prevElement;
+    struct Element * nextElement;
+}Element,*PElement;
+
+typedef struct LinkedQueue{
+       PElement head;
+       PElement tail;
+       int size;
+}LinkedQueue,*PLinkedQueue;
+
 PMatrixGraph matrixExpress();
 void showGraphMatrixGraph(PMatrixGraph pMatrixGraph);
+PElement initElement(int id);
+PLinkedQueue initLinkedQueue();
+PElement outQueue(PLinkedQueue pQueue);
+void showQueue(PLinkedQueue pQueue);
+void showElement(PElement pEle);
+char checkQueue(PLinkedQueue pQueue);
+void inQueue(PLinkedQueue pQueue,PElement pEle);
+void BFS(PMatrixGraph pMatrixGraph);
+
+
 
 /**
  * 
@@ -19,7 +41,17 @@ void showGraphMatrixGraph(PMatrixGraph pMatrixGraph);
  **/ 
 int main(void){
     PMatrixGraph pMatrixGraph = matrixExpress();
-    // showGraphMatrixGraph(pMatrixGraph);
+    BFS(pMatrixGraph);
+    // int flag[7] = {0,0,0,0,0,0,0};
+
+    // PLinkedQueue queue = initLinkedQueue();
+    // inQueue(queue,initElement(0));
+    // inQueue(queue,initElement(1));
+    // inQueue(queue,initElement(2));
+    // showQueue(queue);
+    // outQueue(queue);
+    // printf("\n");
+    // showQueue(queue);
 }
 
 
@@ -36,17 +68,15 @@ int main(void){
  * 
  **/ 
 PMatrixGraph matrixExpress(){
-
     PMatrixGraph pMatrixGraph = (PMatrixGraph)malloc(sizeof(MatrixGraph));
     int nodes[7] = {0,1,2,3,4,5,6};
-
     int len = 7;
     for (int i = 0; i < len; i++){
         pMatrixGraph->nodes[i] = nodes[i];
     }
 
     int edges[7][7] = {{0,1,1,0,0,0,0},
-                       {1,0,0,1,4,0,0},
+                       {1,0,0,1,1,0,0},
                        {1,0,0,0,0,1,0},
                        {0,1,0,0,1,0,1},
                        {0,1,0,1,0,0,0},
@@ -63,6 +93,34 @@ PMatrixGraph matrixExpress(){
     pMatrixGraph->vaildNodeNum = 7;
     showGraphMatrixGraph(pMatrixGraph);
     return pMatrixGraph;
+}
+
+void BFS(PMatrixGraph pMatrixGraph){
+    int flag[7] = {0,0,0,0,0,0,0};
+    PLinkedQueue queue = initLinkedQueue();
+    inQueue(queue,initElement(0));
+    flag[0] = 1;
+    
+    int vaildNodeNum = pMatrixGraph->vaildNodeNum;
+
+    for (int i = 0; i < vaildNodeNum; i++){
+        for (int j = 0; j < vaildNodeNum; j++){
+            int item = pMatrixGraph->edges[i][j];
+            if(item == 0){
+                continue;
+            }
+
+            if(flag[j] == 0){
+                inQueue(queue,initElement(j));
+                flag[j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < vaildNodeNum; i++){
+        PElement pEle = outQueue(queue);
+        printf(" %d ",pEle->id);
+    }
 }
 
 void showGraphMatrixGraph(PMatrixGraph pMatrixGraph){
@@ -88,4 +146,87 @@ void showGraphMatrixGraph(PMatrixGraph pMatrixGraph){
         }
         printf("\n");
     }
+}
+
+PElement initElement(int id){
+    PElement ele = (PElement)malloc(sizeof(Element));
+    ele->id = id;
+    ele->nextElement=NULL;
+    ele->prevElement=NULL;
+    return ele;
+}
+
+PLinkedQueue initLinkedQueue(){
+    PLinkedQueue pQueue = (PLinkedQueue)malloc(sizeof(LinkedQueue));
+    int seq = -1;
+    PElement head = initElement(seq);
+    pQueue->head=head;
+    pQueue->tail=head;
+    pQueue->size=0;
+    return pQueue;
+}
+
+void inQueue(PLinkedQueue pQueue,PElement pEle){
+    if(0 == pQueue->size){
+        free(pQueue->head);
+        pQueue->head=pEle;
+        pQueue->tail=pEle;
+        pQueue->size = pQueue->size+1;
+        return;
+    }
+
+    pQueue->head->prevElement=pEle;
+    pEle->nextElement=pQueue->head;
+    pQueue->head = pEle;
+    pQueue->size = pQueue->size+1;
+    return;
+}
+void showElement(PElement pEle){
+    printf(" %d ",pEle->id);
+}
+
+char checkQueue(PLinkedQueue pQueue){
+    if(NULL == pQueue || 0 == pQueue->size){
+        return 'F';
+    }
+    return 'M';
+}
+
+void showQueue(PLinkedQueue pQueue){
+    char checkResult = checkQueue(pQueue);
+    if('F' == checkResult){
+        printf("队列为空!不给展示!");
+        return;
+    }
+    PElement tempEle = pQueue->head;
+
+    int i = 0;
+    while(i < pQueue->size){
+        showElement(tempEle);
+        tempEle = tempEle->nextElement;
+        i++;
+    }
+}
+
+PElement outQueue(PLinkedQueue pQueue){
+    char checkResult = checkQueue(pQueue);
+    if('F' == checkResult){
+        printf("队列为空，没法儿出队列!");
+        return NULL;
+    }
+
+    PElement pEle = pQueue->tail;
+    if(1 == pQueue->size){
+        pQueue->size = 0;
+        PElement pTempHead = initElement(-1);
+        pQueue->tail=pTempHead;
+        pQueue->head=pTempHead;
+        return pEle;
+    }
+
+    pQueue->tail = pQueue->tail->prevElement;
+    pQueue->size= pQueue->size-1;
+    pEle->prevElement=NULL;
+
+    return pEle;
 }
