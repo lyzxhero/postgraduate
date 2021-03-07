23e5b9176bbbec383bfb194e0d6554e1891c4b42
heroli <lyzx_hero@163.com>
2021-03-07 18:06:37 +0800

    fix

--- data_structure/Sort-quick.c
+++ data_structure/Sort-quick.c
@@ -4,11 +4,15 @@
 
 void quickSort(int arr[],int lowIndex,int hightIndex);
 int adjustArray(int arr[], int lowIndex, int hightIndex);
-
+void showArr(int target[],int len);
 
 
 int main(void){
-
+    int len = 10;
+    int arr[10] = {9,1,2,8,7,6,5,4,0,3};
+    showArr(arr,len);
+    quickSort(arr,0,len-1);
+    showArr(arr,len);
 }
 
 void quickSort(int arr[],int lowIndex,int hightIndex){
@@ -17,7 +21,7 @@ void quickSort(int arr[],int lowIndex,int hightIndex){
     }
 
     int index = adjustArray(arr,lowIndex,hightIndex);
-    quickSort(arr,lowIndex,index);
+    quickSort(arr,lowIndex,index-1);
     quickSort(arr,index+1,hightIndex);
 }
 
@@ -30,7 +34,7 @@ int adjustArray(int arr[], int lowIndex, int hightIndex){
     int pivot = arr[tempLowwerIndex];
     tempLowwerIndex++;
 
-    while(tempLowwerIndex < hightIndex){
+    while(tempLowwerIndex <= hightIndex){
         if(arr[tempLowwerIndex] > pivot){
             upperCount++;
         }else if(arr[tempLowwerIndex] < pivot){
@@ -64,27 +68,47 @@ int adjustArray(int arr[], int lowIndex, int hightIndex){
      * 下列属于既有大于pivot的数又有小于pivot的数
      * 即index在lowIndex和hightIndex之间
      **/
-
     //把pivot放到了对的位置
     int tempNumber = arr[index];
     arr[index] = arr[lowIndex];
     arr[lowIndex] = tempNumber;
 
+    int tempIIndex,tempJIndex;
 
-    int tempPointor = lowIndex;
-    int count = lowwerCount > upperCount ? lowwerCount : upperCount;
-    for(int i = index-1,j=index+1; i >= lowIndex && j<= hightIndex ;){
+    for(int i = index-1,j=index+1; i >= lowIndex || j<= hightIndex ;){
         
-        for (int ii = i; ii < i; ii++){
-            
+        //从中间往左(从大往小)找第一个
+        for (int ii = i; ii >= lowIndex; ii++){
+            if(arr[ii] > pivot){
+                tempIIndex = ii;
+                i--;
+                break;
+            }
+            i--;
         }
 
-        if(arr[j] < pivot){
 
-            j--;
+        for (int jj = j; jj < hightIndex;jj--){
+            if(arr[jj] < pivot){
+                tempJIndex = jj;
+                j++;
+                break;
+            }
+            j++;
         }
+
+        //交换
+        arr[tempIIndex] = arr[tempIIndex] + arr[tempJIndex];
+        arr[tempJIndex] = arr[tempIIndex] - arr[tempJIndex];
+        arr[tempIIndex] = arr[tempIIndex] - arr[tempJIndex];
     }
-    
-    
+
     return index;
+}
+
+void showArr(int target[],int len){
+    for (int i = 0; i < len; i++){
+        printf(" %d ",target[i]);
+    }
+    printf("\n");
 }
